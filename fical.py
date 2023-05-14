81bafb98efa96b0b26b2bb01fcc1fc3d2a8c3e47
ZaRi <zari_09@naver.com>
2023-05-15 02:15:18 +0900

    first start
    보조지표 함수 모음입니다.

---
+++ fical.py
@@ -0,0 +1,49 @@
+# 이동 평균선
+def ma(source, length):     
+    return source.rolling(window=length).mean()
+
+
+# 스토캐스틱
+def fstochastic(source, length):  
+    top = source['close'] - source['low'].rolling(window=length).min()
+    bot = source['high'].rolling(window=length).max() - source['low'].rolling(window=length).min()
+    K = top / bot* 100
+    return K
+
+def fstochastic(close, high, low, length):  
+    top = close - low.rolling(window=length).min()
+    bot = high.rolling(window=length).max() - low.rolling(window=length).min()
+    K = top / bot* 100
+    return K
+
+
+
+
+
+
+
+
+
+"""
+MIT License
+
+Copyright (c) 2023 ZaRi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.  
+"""
